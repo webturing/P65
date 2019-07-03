4c4a6fe67ac07ddc9bf37f90efaf241db9da23d9
webturing <zj@webturing.com>
2019-07-03 10:21:49 +0800

    13  4位反序数 优化枚举，字符串

---
+++ src/p65/P13FourDigitsReverseNumber.java
@@ -0,0 +1,20 @@
+package p65;
+
+public class P13FourDigitsReverseNumber {
+    public static void main(String[] args) {
+        for (int n = 1000; 9 * n <= 9999; n++) {
+            int m = 9 * n;
+            if (Integer.toString(n).equals(new StringBuffer(Integer.toString(m)).reverse().toString())) {
+                System.out.println("The number satisfied states condition is:" + n);
+                System.out.println(n + " * 9 = " + m);
+            }
+        }
+    }
+}
+/**
+ * 13  4位反序数
+ * 设N是一个四位数，它的9倍恰好是其反序数，求N。反序数就是将整数的数字倒过来形成的整数。例如：1234的反序数是4321。
+ * <p>
+ * 运行结果
+ * The number satisfied states condition is:1089
+ */
