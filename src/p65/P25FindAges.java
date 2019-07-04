6b82aeab1ef40e59b92779d0dc6096e0fa1d9df8
webturing <zj@webturing.com>
2019-07-04 08:01:55 +0800

    P25年龄几何 存在单调性可以二分答案

--- src/p65/P25FindAges.java
+++ src/p65/P25FindAges.java
@@ -1,10 +1,7 @@
 package p65;
 
-import java.util.Arrays;
-
 public class P25FindAges {
     public static void main(String[] args) {
-        int a[] = new int[4];
         int left = 0, right = 100;
         int d = 0;
         while (left <= right) {
@@ -15,11 +12,7 @@ public class P25FindAges {
                 continue;
             }
             if (4 * mid + 6 * d == 26 && mid * (mid + d) * (mid + 2 * d) * (mid + 3 * d) == 880) {
-                a[0] = mid;
-                a[1] = mid + d;
-                a[2] = a[1] + d;
-                a[3] = a[2] + d;
-                System.out.println(Arrays.toString(a));
+                System.out.println(String.format("%d %d %d %d", mid, mid + d, mid + 2 * d, mid + 3 * d));
                 break;
             } else if (4 * mid + 6 * d < 26 || mid * (mid + d) * (mid + 2 * d) * (mid + 3 * d) < 880) {
                 left = mid + 1;
@@ -29,3 +22,11 @@ public class P25FindAges {
         }
     }
 }
+/**
+ * 25年龄几何
+ * 张三、李四、王五、刘六的年龄成一等差数列，他们四人的年龄相加是26，相乘是880，求以他们的年龄为前4项的等差数列的前20项。
+ * <p>
+ * 运行结果
+ * The series with equal difference are:
+ * 2 5 8 11 14 17 20 23 26 29 32 35 38 41 44 47 50 53 56 59
+ */
