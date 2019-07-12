a4b7abd0971eb4a6c2cc1c7c072f98a1e06cabb7
webturing <zj@webturing.com>
2019-07-12 11:55:14 +0800

    53奇数平方的一个有趣性质:编程验证“大于1000的奇数其平方与1的差是8的倍数”。

--- src/P53TheoremVerification.java
+++ src/P53TheoremVerification.java
@@ -3,9 +3,8 @@ public class P53TheoremVerification {
         //(2*i+1)^2-1=4*i^2+4i+1-1=4*i(i+1) ok
         for (int i = 1001; ; i += 2) {
             long l = 1L * i * i - 1;
-            System.out.println(l % 8);
+            System.out.println("for i=" + i + ", It is " + (l % 8 == 0));
         }
     }
 }
-//53奇数平方的一个有趣性质
-//        编程验证“大于1000的奇数其平方与1的差是8的倍数”。
+//53奇数平方的一个有趣性质:编程验证“大于1000的奇数其平方与1的差是8的倍数”。
